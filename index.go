51a367f692f64aa147dbc5430adb0c15071da37d
Danny Hermes <daniel.j.hermes@gmail.com>
2015-01-19 17:59:31 -0800

    Adding login redirect.

--- index.go
+++ index.go
@@ -6,6 +6,9 @@ import _ "appengine/remote_api"
 import (
 	"html/template"
 	"net/http"
+
+	"appengine"
+	"appengine/user"
 )
 
 var (
@@ -17,7 +20,7 @@ var (
 	))
 )
 
-type IndexData struct {
+type indexData struct {
 	Id        string
 	Calendars string
 	Frequency string
@@ -28,8 +31,24 @@ func init() {
 	http.HandleFunc("/", indexOr404)
 }
 
+func loginRedirect(w http.ResponseWriter, c appengine.Context, r *http.Request) {
+	url, err := user.LoginURL(c, r.URL.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Location", url)
+	w.WriteHeader(http.StatusFound)
+}
+
 func index(writer http.ResponseWriter, request *http.Request) {
-	data := IndexData{Id: "Foo", Calendars: "Bar", Frequency: "Baz"}
+	c := appengine.NewContext(request)
+	u := user.Current(c)
+	if u == nil {
+		loginRedirect(writer, c, request)
+		return
+	}
+
+	data := indexData{Id: u.Email, Calendars: "[]", Frequency: "[]"}
 	err := templateIndex.Execute(writer, data)
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
