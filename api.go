466eda03787c4bd1ae0a20c6a97b592eb5138678
Danny Hermes <daniel.j.hermes@gmail.com>
2015-01-20 23:20:58 -0800

    Only accepting correct verbs in API methods.

--- api.go
+++ api.go
@@ -5,20 +5,44 @@ import (
 	"net/http"
 )
 
+var notAllowed405 = `<html>
+  <head>
+    <title>405 Method Not Allowed</title>
+  </head>
+</html>
+`
+
 func init() {
 	http.HandleFunc("/add", addSubscription)
 	http.HandleFunc("/freq", changeFrequency)
 	http.HandleFunc("/getinfo", getInfo)
 }
 
-func addSubscription(writer http.ResponseWriter, unusedReq *http.Request) {
+func notAllowed(writer http.ResponseWriter) {
+	fmt.Fprint(writer, notAllowed405)
+	writer.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func addSubscription(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "POST" {
+		notAllowed(writer)
+		return
+	}
 	fmt.Fprint(writer, `"no_user:fail"`)
 }
 
-func changeFrequency(writer http.ResponseWriter, unusedReq *http.Request) {
+func changeFrequency(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "PUT" {
+		notAllowed(writer)
+		return
+	}
 	fmt.Fprint(writer, `"no_user:fail"`)
 }
 
-func getInfo(writer http.ResponseWriter, unusedReq *http.Request) {
+func getInfo(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		notAllowed(writer)
+		return
+	}
 	fmt.Fprint(writer, `"no_user:fail"`)
 }
